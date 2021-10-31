4d98df329c99522212d2479765b38dc3b336d6d6
Btrnq <b.tronquoy@gmail.com>
2021-10-31 15:21:41 +0100

    (update) change version number

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "author" : "Otmar Nitsche",
     "description" : "This toolkit prepares your 3D assets to be used for Microsoft Flight Simulator. Copyright (c) 2020 Otmar Nitsche",
     "blender" : (2, 82, 3),
-    "version" : (0, 41, 3),
+    "version" : (0, 41, 4),
     "location" : "View3D",
     "warning" : "This version of the addon is work-in-progress. Don't use it in your active development cycle, as it adds variables and objects to the scene that may cause issues further down the line.",
     "category" : "3D View",
